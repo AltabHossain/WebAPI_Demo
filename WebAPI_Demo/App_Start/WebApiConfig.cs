a2120e1cf0e835ce075416e668ec36df461c15eb
Altab Hossain <altab_ru@yahoo.com>
2018-05-03 13:12:08 -0400

    Web API

--- WebAPI_Demo/App_Start/WebApiConfig.cs
+++ WebAPI_Demo/App_Start/WebApiConfig.cs
@@ -15,6 +15,12 @@ namespace WebAPI_Demo
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+               name: "ActionApi",
+               routeTemplate: "api/{controller}/{action}/{id}",
+               defaults: new { id = RouteParameter.Optional }
+           );
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
